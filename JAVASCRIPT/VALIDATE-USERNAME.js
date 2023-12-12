c9494d387e0ae9c62d9553d12b00a5592a51c6b4
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-12-12 11:00:03 +0530

    feat:Validate-UsernName

---
+++ JAVASCRIPT/VALIDATE-USERNAME.js
@@ -0,0 +1,29 @@
+// Write a simple regex to validate a username. Allowed characters are:
+//     lowercase letters,
+//     numbers,
+//     underscore
+// Length should be between 4 and 16 characters (both included).
+
+
+function validateUser(username){
+        let res = /^[a-z0-9_]{4,16}$/.test(username)
+        console.log(res)    
+}
+
+
+
+
+
+
+
+
+// Note
+// /: Delimiters.regular expressions are enclosed in forward slashes to indicate the beginning and end of the pattern.
+// ^: Anchors the regex at the beginning of the string. It asserts that the following pattern should match at the start of the string.
+// [a-z0-9_]: set of characters allowed
+// a-z: lowercase letter from 'a' to 'z'.
+// 0-9:  digit from '0' to '9'.
+// _: The underscore character.
+// $: Anchors the regex at the end of the string.
+
+
